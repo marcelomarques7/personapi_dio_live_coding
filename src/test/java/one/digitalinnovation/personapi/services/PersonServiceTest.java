49272d5da3fb2fdbcf5a82cfe15e6c7f2165b13e
marcelomarques7 <egs.marcelo@gmail.com>
2021-10-08 00:42:16 -0300

    Atualizando os teste unit

--- src/test/java/one/digitalinnovation/personapi/services/PersonServiceTest.java
+++ src/test/java/one/digitalinnovation/personapi/services/PersonServiceTest.java
@@ -1,8 +1,7 @@
 package one.digitalinnovation.personapi.services;
 
-import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
-import one.digitalinnovation.personapi.dto.mapper.PersonMapper;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
+import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.entities.Person;
 import one.digitalinnovation.personapi.repositories.PersonRepository;
 import org.junit.jupiter.api.Test;
@@ -11,11 +10,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static one.digitalinnovation.personapi.utils.PersonUtils.createFakeDTO;
-import static one.digitalinnovation.personapi.utils.PersonUtils.createFakeEntity;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.when;
+import static one.digitalinnovation.personapi.utils.PersonUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class PersonServiceTest {
@@ -23,30 +20,26 @@ public class PersonServiceTest {
     @Mock
     private PersonRepository personRepository;
 
-    @Mock
-    private PersonMapper personMapper;
-
     @InjectMocks
     private PersonService personService;
 
     @Test
-    void testGivenPersonDTOThenReturnSuccessSavedMessage() {
+    void testGivenPersonDTOThenReturnSavedMessage() {
         PersonDTO personDTO = createFakeDTO();
         Person expectedSavedPerson = createFakeEntity();
 
-        when(personMapper.toModel(personDTO)).thenReturn(expectedSavedPerson);
         when(personRepository.save(any(Person.class))).thenReturn(expectedSavedPerson);
 
-        MessageResponseDTO expectedSuccessMessage = createExpectedSuccessMessage(expectedSavedPerson.getId());
-        MessageResponseDTO successMessage = personService.create(personDTO);
+        MessageResponseDTO expectedSuccessMessage = createExpectedMessageResponse(expectedSavedPerson.getId());
+        MessageResponseDTO succesMessage = personService.create(personDTO);
 
-        assertEquals(expectedSuccessMessage, successMessage);
+        assertEquals(expectedSuccessMessage, succesMessage);
     }
 
-    private MessageResponseDTO createExpectedSuccessMessage(Long savedPersonId) {
-        return MessageResponseDTO.builder()
-                .message("Person successfully created with ID " + savedPersonId)
+    private MessageResponseDTO createExpectedMessageResponse(Long id) {
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + id)
                 .build();
     }
-
 }
